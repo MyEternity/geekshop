a7a698e553fc80ab2e4e31e42e69067225b92738
MyEternity (Home) <spddarkforce47@gmail.com>
2021-09-19 00:50:35 +0300

    Lesson 2 Templates + And Links

--- products/urls.py
+++ products/urls.py
@@ -14,14 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
-from products.views import index
+from .views import products
 
+app_name = 'products'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='index'),
-    path('products/', include('products.urls', namespace='pr')),
+    path('', products, name='index'),
 
 ]
