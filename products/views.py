d7354e23da89ca1108e49b801d6b13cfbff64ab9
MyEternity (Home) <spddarkforce47@gmail.com>
2021-09-19 01:47:24 +0300

    Lesson 2 Date + Loading from JSON

--- products/views.py
+++ products/views.py
@@ -1,12 +1,31 @@
+import datetime
+import json
+import os.path
+
 from django.shortcuts import render
+
+SITE_TITLE = 'geeks'
 
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': SITE_TITLE,
+        'current_date': datetime.datetime.now().strftime("%d/%m/%Y")
+    }
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    def readfile(file_name):
+        with open(file_name, 'r', encoding='utf-8') as f:
+            return json.load(f)
+
+    context = {
+        'title': f'{SITE_TITLE}: Каталог',
+        'current_date': datetime.datetime.now().strftime("%d/%m/%Y"),
+        'products': readfile(os.path.join(os.path.dirname(__file__), 'fixtures/products.json'))
+    }
+    return render(request, 'products/products.html', context)
 
 
 def test(request):
