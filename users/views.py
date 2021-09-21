1c72c860ba7b28d5d46a863b9e80b44f0013323d
MyEternity (Home) <spddarkforce47@gmail.com>
2021-09-21 21:46:31 +0300

    Lesson 4 Added + Enabled user login/register pages.

--- users/views.py
+++ users/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def login(request):
+    context = {
+        'title': 'GS: Авторизация'
+    }
+    return render(request, 'users/login.html', context)
+
+
+def register(request):
+    context = {
+        'title': 'GS: Регистрация'
+    }
+    return render(request, 'users/register.html', context)
