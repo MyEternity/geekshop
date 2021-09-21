0f9a8e5eeb0cdfaa80c25e0cb9195ef95d4e9315
MyEternity (Home) <spddarkforce47@gmail.com>
2021-09-21 22:44:41 +0300

    Lesson 4 Login

--- users/views.py
+++ users/views.py
@@ -1,12 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib import auth
+from django.urls import reverse
 
 from users.forms import UserLoginForm
+
+
 # Create your views here.
 
 
 def login(request):
-    form = UserLoginForm()
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+        else:
+            print(form.errors)
+    else:
+        form = UserLoginForm()
     context = {
         'title': 'GS: Авторизация',
         'form': form
