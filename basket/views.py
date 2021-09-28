f4ad13562b08134ad0dbc4fbf1cf70094889ed99
MyEternity (Home) <spddarkforce47@gmail.com>
2021-09-28 21:37:28 +0300

    Lesson 6 + login decorator

--- basket/views.py
+++ basket/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.shortcuts import HttpResponseRedirect
@@ -9,7 +10,7 @@ from basket.models import Basket
 
 # Create your views here.
 
-
+@login_required
 def basket_add(request, product_id):
     user_select = request.user
     product = Product.objects.get(id=product_id)
@@ -25,11 +26,13 @@ def basket_add(request, product_id):
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required
 def basket_remove(request, product_id):
     Basket.objects.get(id=product_id).delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required
 def basket_edit(request, id, quantity):
     if request.is_ajax():
         basket = Basket.objects.get(id=id)
@@ -38,9 +41,10 @@ def basket_edit(request, id, quantity):
             basket.save()
         else:
             basket.delete()
-    basket = Basket.objects.filter(user=request.user)
-    context = {
-        'basket': basket
-    }
-    result = render_to_string('basket/basket.html', context)
-    return JsonResponse({'result': result})
+
+        basket = Basket.objects.filter(user=request.user)
+        context = {
+            'basket': basket
+        }
+        result = render_to_string('basket/basket.html', context)
+        return JsonResponse({'result': result})
