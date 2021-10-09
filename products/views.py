6ce793f17870d8b962447eefdaccb029adbca48c
MyEternity (Home) <spddarkforce47@gmail.com>
2021-10-09 17:18:41 +0300

    Lesson 8 Index CBV

--- products/views.py
+++ products/views.py
@@ -1,16 +1,46 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
+from django.views.generic import FormView
 
 from .models import Product, ProductCategory
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 SITE_TITLE = 'geeks'
 
 
-def index(request):
-    context = {
-        'title': SITE_TITLE
-    }
-    return render(request, 'products/index.html', context)
+class IndexView(FormView):
+    form_class = FormView
+    template_name = 'products/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['title'] = SITE_TITLE
+        return context
+
+
+# class ProductsView(FormView):
+#     form_class = FormView
+#     template_name = 'products/products.html'
+#
+#     def get_context_data(self, **kwargs):
+#         context = super(ProductsView, self).get_context_data(**kwargs)
+#         context['title'] = SITE_TITLE
+#
+#         if self.request.code is None:
+#             selected_products = Product.objects.all()
+#         else:
+#             selected_products = Product.objects.filter(category_id=self.request.code)
+#
+#         paginator = Paginator(selected_products, per_page=3)
+#         try:
+#             pp = paginator.page(self.request.page_id)
+#         except PageNotAnInteger:
+#             pp = paginator.page(1)
+#         except EmptyPage:
+#             pp = paginator.page(paginator.num_pages)
+#
+#         context['products'] = pp
+#         context['categories'] = ProductCategory.objects.all()
+#         return context
 
 
 def products(request, code=None, page_id=1):
