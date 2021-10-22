5d718fbe54f0436618066e47e012acfcc1d1decf
MyEternity (Home) <spddarkforce47@gmail.com>
2021-10-22 08:26:14 +0300

    git ignore

--- geekshop/mixin.py
+++ geekshop/mixin.py
@@ -20,6 +20,6 @@ class BaseClassContextMixin(ContextMixin):
     title = ''
 
     def get_context_data(self, **kwargs):
-        context = super(BaseClassContextMixin, self).get_context_data(**kwargs)
-        context.title = self.title
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.title
         return context
